6be83c2f954e444afcb2c507637faf7641be1518
ansu-kri <ansukummari@gmail.com>
2026-05-09 17:05:28 +0530

    updating server app for production ready

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();   // must be first
+
+const connectDB = require("./src/Configs/db");
+const app = require("./src/app");
+
+const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ backend/src/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors")
+const dotenv = require("dotenv");
+const authRoutes = require("./Routes/routes");
+
+const app = express();
+
+//CORS configuation (Production safe)
+const allowedOrigins =[
+    "http://localhost:3000", //local dev
+    process.env.FRONTEND_URL, // production frontend
+];
+
+app.use(
+    cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+//parse urlencoded form data(need for multer)
+app.use(express.urlencoded({ extended: true}));
+//serve uploads folder statically
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api", authRoutes);
+
+module.exports = app;
